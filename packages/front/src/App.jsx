eb5393feddd0e66e07fe1f8e49582e5afaccb98c
rolled-potatoes <keep4404@gmail.com>
2022-07-09 20:55:56 +0900

    feat: Add Auth check router

--- packages/front/src/App.jsx
+++ packages/front/src/App.jsx
@@ -1,9 +1,38 @@
-import React from 'react';
-import { Routes, BrowserRouter, Route } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Routes, BrowserRouter, Route, useNavigate } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import Authentication from 'pages/Authentication';
 import navLink from 'constants/navLink';
+
+import { useQuery } from 'react-query';
+import { Spinner } from 'reactstrap';
+import { getAuth } from 'apis/auth';
 const queryClient = new QueryClient();
+
+function AuthRoute({ children }) {
+  const navigate = useNavigate();
+
+  const { isLoading, data } = useQuery(
+    'key',
+    () => getAuth().then((res) => res.data),
+    {
+      refetchOnWindowFocus: false,
+    }
+  );
+
+  useEffect(() => {
+    if (!isLoading && !data) {
+      navigate('/login', { replace: true });
+    }
+  }, [isLoading, data]);
+
+  return (
+    <>
+      {isLoading && <Spinner />}
+      {!isLoading && children}
+    </>
+  );
+}
 
 function App() {
   return (
@@ -11,8 +40,15 @@ function App() {
       <Authentication>
         <BrowserRouter>
           <Routes>
-            {navLink.map(({ url, Component }) => {
-              return <Route key={url} path={url} element={<Component />} />;
+            {navLink.map(({ url, Component, isAuth }) => {
+              const element = isAuth ? (
+                <AuthRoute>
+                  <Component />
+                </AuthRoute>
+              ) : (
+                <Component />
+              );
+              return <Route key={url} path={url} element={element} />;
             })}
           </Routes>
         </BrowserRouter>
--- packages/front/src/constants/navLink.js
+++ packages/front/src/constants/navLink.js
@@ -6,11 +6,13 @@ const navigationDatas = [
     url: 'login',
     title: '로그인',
     Component: Login,
+    isAuth: false,
   },
   {
     url: '/',
     title: '메인',
     Component: Main,
+    isAuth: true,
   },
 ];
 
